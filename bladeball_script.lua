a48485dcdd8ebebb8de9499feea94090a61ae976
scriptjame <132070412+scriptjame@users.noreply.github.com>
2026-03-15 01:44:26 +0700

    Update bladeball_script.lua

--- bladeball_script.lua
+++ bladeball_script.lua
@@ -178,8 +178,8 @@ free.MouseButton1Click:Connect(function()
 
 	task.wait(0.3)
 
-	loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/987304be42d04f975daf2efce8130d7a.lua"))()
-
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaw/Plutonium.AA/main/Plutonium.Loader.lua", true))()
+		
 end)
 
 -- LOOTLABS BUTTON
